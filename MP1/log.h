976ded2d32abb3bbfc73661587e5033bf95eeac6
Dev-Kanani <143739012+Dev-Kanani@users.noreply.github.com>
2025-07-09 13:21:40 +0530

    Delete MP1 directory

--- MP1/log.h
+++
@@ -1,9 +0,0 @@
-#ifndef LOG_H
-#define LOG_H
-
-#include "all_directories.h"
-
-void log_command(char *command, int *flag, int execute_flag);
-void log_execute(char *newCommand, int number);
-
-#endif
--- MP1/proclore.h
+++
@@ -1,6 +0,0 @@
-#ifndef PROCLORE_H
-#define PROCLORE_H
-
-void get_process_info(int pid, char *currentDirectory, char *newHomeDirectory);
-
-#endif
